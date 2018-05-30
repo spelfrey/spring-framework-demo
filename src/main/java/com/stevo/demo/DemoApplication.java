303a0b3a228d517d35a931bc9c2b479c310eb966
Stephen Pelfrey <spelfrey@outlook.com>
2018-05-30 17:58:44 -0500

    More commits

---
+++ src/main/java/com/stevo/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.stevo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+    git config --global user.email "spelfrey@outlook.com"
+
+        git config --global user.name "Stephen Pelfrey"
