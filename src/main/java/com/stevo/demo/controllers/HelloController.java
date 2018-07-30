9313671c8cd7c98d5f1717e85763a8f5ae9da5eb
Stephen Pelfrey <stephen.pelfrey@daveramsey.com>
2018-07-30 12:34:59 -0500

    Using PathVariables and RequestParams

--- src/main/java/com/stevo/demo/controllers/HelloController.java
+++ src/main/java/com/stevo/demo/controllers/HelloController.java
@@ -1,13 +1,12 @@
 package com.stevo.demo.controllers;
 
 import com.stevo.demo.utils.MyUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -23,5 +22,16 @@ public class HelloController {
         model.addAttribute("name", MyUtils.getMessage("text", "http://below18.example.com",
                 "http://above18.example.com", LocaleContextHolder.getLocale()));
         return "hello";
+    }
+
+    @RequestMapping("/hello/{id}")
+    public String helloId(Model model,
+            @PathVariable("id") int id,
+            @RequestParam("name") String name) {
+
+        model.addAttribute("id", id);
+        model.addAttribute("name", name);
+
+        return "hello-id";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/hello-id.jsp
@@ -0,0 +1,12 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: spelf
+  Date: 7/4/2018
+  Time: 7:51 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@include file="includes/header.jsp" %>
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<spring:message code="hello"/>, ${name}, your id is ${id}!
+<%@include file="includes/footer.jsp" %>
