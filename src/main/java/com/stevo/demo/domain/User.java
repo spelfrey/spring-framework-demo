7aa6c582cebbebedb6d69618753265ab54cd44aa
Stephen Pelfrey <spelfrey@users.noreply.github.com>
2021-09-13 20:50:36 -0500

    Section 2.4 Spring Data Enum & Collection (#4)
    * Section 2.4 Spring Data Enum & Collection
    
    * Section 2.4 Enum of string

--- src/main/java/com/stevo/demo/domain/User.java
+++ src/main/java/com/stevo/demo/domain/User.java
@@ -1,7 +1,13 @@
 package com.stevo.demo.domain;
 
+import java.util.Collection;
+import java.util.HashSet;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -9,6 +15,12 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "usr")
 public class User {
+
+  public enum Role {
+    UNVERIFIED,
+    ADMIN,
+    BLOCKED
+  }
 
   @Id @GeneratedValue private Long id;
 
@@ -20,6 +32,10 @@ public class User {
 
   @Column(nullable = false)
   private String password;
+
+  @ElementCollection(fetch = FetchType.EAGER)
+  @Enumerated(EnumType.STRING)
+  private Collection<Role> roles = new HashSet<Role>();
 
   public Long getId() {
     return id;
@@ -51,5 +67,13 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public Collection<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Collection<Role> roles) {
+    this.roles = roles;
   }
 }
