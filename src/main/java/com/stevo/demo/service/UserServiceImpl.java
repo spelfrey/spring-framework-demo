02cb03fd89d95fa8a9b8e1d7b5153c928d43e776
Stephen Pelfrey <stephen.pelfrey@daveramsey.com>
2021-09-29 20:31:01 -0500

    4.11 Creating an Administrator

--- src/main/java/com/stevo/demo/service/UserServiceImpl.java
+++ src/main/java/com/stevo/demo/service/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.stevo.demo.service;
 
+import static com.stevo.demo.domain.User.Role.ADMIN;
 import static com.stevo.demo.domain.User.Role.UNVERIFIED;
 
 import com.stevo.demo.commands.UserCommand;
 import com.stevo.demo.domain.User;
 import com.stevo.demo.repositories.UserRepository;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
+
+  @Value("${admin.email}")
+  private String adminEmail;
+
+  @Value("${admin.name}")
+  private String adminName;
+
+  @Value("${admin.password}")
+  private String adminPassword;
 
   private UserRepository userRepository;
 
@@ -31,8 +43,16 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @EventListener
+  @Transactional
   public void afterApplicationReady(ApplicationReadyEvent applicationReadyEvent) {
-    log.info("Inside afterApplicationReady");
+    if (!userRepository.findByEmail(adminEmail).isPresent()) {
+      User user = new User();
+      user.setEmail(adminEmail);
+      user.setName(adminName);
+      user.setPassword(adminPassword);
+      user.setRoles(List.of(ADMIN));
+      userRepository.save(user);
+    }
   }
 
   @Override
