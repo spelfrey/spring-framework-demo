6a2afea841cd75aa89435f611fa2a9af8561f285
Stephen Pelfrey <stephen.pelfrey@daveramsey.com>
2021-09-29 20:16:43 -0500

    4.10 Running code at startup

--- src/main/java/com/stevo/demo/service/UserService.java
+++ src/main/java/com/stevo/demo/service/UserService.java
@@ -1,8 +1,11 @@
 package com.stevo.demo.service;
 
 import com.stevo.demo.commands.UserCommand;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 
 public interface UserService {
 
   void signup(UserCommand userCommand);
+
+  void afterApplicationReady(ApplicationReadyEvent applicationReadyEvent);
 }
--- src/main/java/com/stevo/demo/service/UserServiceImpl.java
+++ src/main/java/com/stevo/demo/service/UserServiceImpl.java
@@ -5,6 +5,11 @@ import static com.stevo.demo.domain.User.Role.UNVERIFIED;
 import com.stevo.demo.commands.UserCommand;
 import com.stevo.demo.domain.User;
 import com.stevo.demo.repositories.UserRepository;
+import javax.annotation.PostConstruct;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +18,21 @@ public class UserServiceImpl implements UserService {
 
   private UserRepository userRepository;
 
+  private static Log log = LogFactory.getLog(UserServiceImpl.class);
+
   public UserServiceImpl(UserRepository userRepository) {
     this.userRepository = userRepository;
+  }
+
+  @PostConstruct
+  public void init() {
+    log.info("Inside PostConstructor");
+  }
+
+  @Override
+  @EventListener
+  public void afterApplicationReady(ApplicationReadyEvent applicationReadyEvent) {
+    log.info("Inside afterApplicationReady");
   }
 
   @Override
